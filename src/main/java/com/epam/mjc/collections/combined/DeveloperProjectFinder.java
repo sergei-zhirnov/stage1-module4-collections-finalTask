d833b665944afde8db26bf65ec3fc4431c75899c
sergei-zhirnov <s.zhirnov@protonmail.com>
2023-12-05 21:55:20 +0400

    Stage 1, Module 4, Final Task, Attempt 1

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,29 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> entry: projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                result.add(entry.getKey());
+            }
+        }
+
+        Comparator<String> localSorting = (o1, o2) -> {
+            if (o1.length() > o2.length()) {
+                return -1;
+            } else if (o1.length() < o2.length()) {
+                return 1;
+            } else {
+                return -o1.compareTo(o2);
+            }
+        };
+
+        result.sort(localSorting);
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,28 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+
+        Set<String> origKeys = sourceMap.keySet();
+        Set<Integer> newKeys = new HashSet<>();
+
+        for (String s: origKeys) {
+            newKeys.add(s.length());
+        }
+
+        for (int i: newKeys) {
+            Set<String> newValues = new HashSet<>();
+            for (String s: origKeys) {
+                if (s.length() == i) {
+                    newValues.add(s);
+                }
+            }
+            result.put(i, newValues);
+        }
+
+        return  result;
     }
 }
